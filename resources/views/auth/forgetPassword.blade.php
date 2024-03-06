1f40a39b4e6f7f37cd248ed8a3588791ce07cc26
lahcen <lahcen.ben3@gmail.com>
2024-03-06 22:13:16 +0100

    added forget password with email verify

---
+++ resources/views/auth/forgetPassword.blade.php
@@ -0,0 +1,61 @@
+@extends('auth.layout')
+
+@section('content')
+    <div class="min-h-screen flex items-center justify-center w-full dark:bg-gray-950">
+        <div class="bg-white dark:bg-gray-900 shadow-md rounded-lg px-8 py-6 max-w-md" style="min-width: 35%;">
+            <h1 class="text-2xl font-bold text-center mb-4 dark:text-gray-200">Forgot Password</h1>
+            <form action="{{ route('forget.password.post') }}" method="POST">
+                @csrf
+                <div class="mb-4">
+                    <label for="email" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Email
+                        Address</label>
+                    <input type="email" id="email" name="email"
+                        class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                        placeholder="your@email.com" required>
+                    @error('email')
+                        <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                    @enderror
+                </div>
+                <button type="submit"
+                    class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Send
+                    Password Reset Link</button>
+            </form>
+        </div>
+    </div>
+    {{-- <div class="antialiased bg-slate-200">
+        <div class="max-w-lg mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-300">
+            <h1 class="text-4xl font-medium">Reset password</h1>
+            <p class="text-slate-500">Fill up the form to reset the password</p>
+
+            <form action="" class="my-10">
+                <div class="flex flex-col space-y-5">
+                    <label for="email">
+                        <p class="font-medium text-slate-700 pb-2">Email address</p>
+                        <input id="email" name="email" type="email"
+                            class="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
+                            placeholder="Enter email address">
+                    </label>
+
+                    <button
+                        class="w-full py-3 font-medium text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z" />
+                        </svg>
+
+                        <span>Reset password</span>
+                    </button>
+                    <p class="text-center">Not registered yet? <a href="#"
+                            class="text-indigo-600 font-medium inline-flex space-x-1 items-center"><span>Register now
+                            </span><span><svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                                </svg></span></a></p>
+                </div>
+            </form>
+        </div>
+
+    </div> --}}
+@endsection
