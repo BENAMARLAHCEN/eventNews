ead9f7a5b157a2eaf7837028b6269eb42d796cf3
lahcen <lahcen.ben3@gmail.com>
2024-03-06 22:01:05 +0100

    add permission and role model, create relationships

---
+++ app/Models/Role.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class, 'roles_permissions');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'users_roles');
+    }
+
+    public function givePermissionsTo(...$permissions)
+    {
+        $permissions = $this->getAllPermissions($permissions);
+        if ($permissions === null) {
+            return $this;
+        }
+        $this->permissions()->saveMany($permissions);
+        return $this;
+    }
+    
+    public function refreshPermissions(...$permissions)
+    {
+        $this->permissions()->detach();
+        return $this->givePermissionsTo($permissions);
+    }
+
+    protected function getAllPermissions(array $permissions)
+    {
+        return Permission::whereIn('name', $permissions)->get();
+    }
+
+
+    public function hasPermission(...$permissions)
+    {
+        foreach ($permissions as $permission) {
+            if ($this->permissions->contains('name', $permission)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+}
