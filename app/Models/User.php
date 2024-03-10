160213c2e4d2439588c195de3b1ce80c9e4d8d3e
lahcen <lahcen.ben3@gmail.com>
2024-03-10 17:46:23 +0000

    Add isBanned() and banned() methods to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use App\Traits\HasPermissionsTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -67,5 +68,14 @@ class User extends Authenticatable
         return $this->hasMany(Reservation::class);
     }
 
+    public function isBanned()
+    {
+        return DB::table('banned_users')->where('user_id', $this->id)->exists();
+    }
+
+    public function banned($reason){
+       return DB::table('banned_users')->insert(['user_id' => $this->id, 'reason' => $reason]);
+    }
+    
 
 }
