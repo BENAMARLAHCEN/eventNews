a28953a9836a85c23ab5918d4c986b1489feda6d
lahcen <lahcen.ben3@gmail.com>
2024-03-10 14:08:41 +0000

    Update EventRepository methods to use a default pagination value of 8

--- app/Repository/EventRepository.php
+++ app/Repository/EventRepository.php
@@ -7,42 +7,26 @@ use App\Repository\Interface\IEventRepository;
 
 class EventRepository implements IEventRepository
 {
-    public function list(int $perPage = 10)
+    public function list(int $perPage = 8)
     {
         return Event::latest()->paginate($perPage);
     }
-
-    // Schema::create('events', function (Blueprint $table) {
-    //     $table->id();
-    //     $table->string('title');
-    //     $table->text('description');
-    //     $table->dateTime('event_date');
-    //     $table->string('location');
-    //     $table->enum('reservation_approval_mode', ['automatic', 'manual'])->default('automatic');
-    //     $table->unsignedBigInteger('category_id');
-    //     $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
-    //     $table->unsignedBigInteger('organizer_id');
-    //     $table->foreign('organizer_id')->references('id')->on('users')->onDelete('cascade');
-    //     $table->integer('capacity');
-    //     $table->timestamps();
-    // });
-
-    public function listPending(int $perPage = 10)
+    public function listPending(int $perPage = 8)
     {
         return Event::latest()->where('status', 'pending')->paginate($perPage);
     }
 
-    public function listByUser($userId, $perPage = 10)
+    public function listByUser($userId, $perPage = 8)
     {
         return Event::latest()->where('organizer_id', $userId)->paginate($perPage);
     }
 
-    public function listPublished(int $perPage = 10)
+    public function listPublished(int $perPage = 8)
     {
         return Event::latest()->where('status', 'published')->paginate($perPage);
     }
 
-    public function listRejected(int $perPage = 10)
+    public function listRejected(int $perPage = 8)
     {
         return Event::latest()->where('status', 'draft')->paginate($perPage);
     }
@@ -81,5 +65,17 @@ class EventRepository implements IEventRepository
     {
         $event = $this->findById($id);
         $event->delete();
+    }
+
+    public function search($search, $category = null)
+    {
+        $query = Event::query();
+        if ($search || $category != null) {
+            $query->where('title', 'like', "%$search%");
+        }
+        if ($category != null) {
+            $query->whereIn('category_id', $category);
+        }
+        return $query->get(); 
     }
 }
