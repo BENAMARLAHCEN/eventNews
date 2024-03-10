1c8b3dfd6b0241907787c0ea7bb297b51c342e7e
lahcen <lahcen.ben3@gmail.com>
2024-03-10 17:03:56 +0000

    Refactor user permission display in index.blade.php

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -44,7 +44,7 @@
                                         @foreach ($user->userPermissions() as $permission)
                                             <span
                                                 class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-indigo-500 rounded-full">
-                                                {{ $permission['name'] }}
+                                                {{ $permission }}
                                             </span>
                                         @endforeach
 
