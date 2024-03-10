31661acc9715b4d31897f919a7b52a1630582f35
lahcen <lahcen.ben3@gmail.com>
2024-03-10 22:39:08 +0000

    Update user authentication logic

--- resources/views/tickets/ticket.blade.php
+++ resources/views/tickets/ticket.blade.php
@@ -1,231 +1,209 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+
+    <style>
+        /* Add your custom styles here */
+
+        * {
+            box-sizing: border-box;
+            font-family: sans-serif;
+        }
+
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .ticket {
+            width: 95%;
+            padding: 0.3cm;
+            border: 2px solid #ddd;
+            background-color: rgba(242, 223, 242, 0.893);
+            display: flex;
+            flex-direction: column;
+        }
+
+        .logo {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        .client-name{
+            font-weight: 700
+        }
+
+        .fill-violet-400 {
+            fill: rgb(188, 79, 188)
+        }
+
+        .event-info {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        .event-date {
+            font-size: 1rem;
+            background-color: rgb(249, 155, 249);
+            color: #007820;
+            padding: 5px;
+            border-radius: 7px;
+        }
+
+        .event-title {
+            font-size: 18px;
+            font-weight: bold;
+        }
+
+        .event-details {
+            font-size: 14px;
+            color: #777;
+            margin-bottom: 10px;
+        }
+
+        .ticket-price {
+            background-color: #eee;
+            padding: 5px 10px;
+            border-radius: 4px;
+            font-weight: bold;
+        }
+    </style>
 </head>
 
-<body>
-    <div class="cardWrap">
-        <div class="card cardLeft">
-            <h1>Event <span>News</span></h1>
-            <div class="title">
-                <h2>{{$reservation->event->title}}</h2>
-                <span>{{$reservation->event->category->name}}</span>
-            </div>
-            <div class="name">
-                <h2>{{$reservation->user->name}}</h2>
-                <span>name</span>
-            </div>
-            <div class="seat">
-                <h2>{{$reservation->event->capacity}}</h2>
-                <span>seat</span>
-            </div>
-            <div class="time">
-                <h2>{{ \Carbon\Carbon::parse($reservation->event->date)->format('d M, Y') }},<p>{{ \Carbon\Carbon::parse($reservation->event->date)->format('h:i') }}</p> </h2>
-                <span>time</span>
-            </div>
-
+<body style="width: 100%;">
+    <div class="ticket">
+        <div class="event-info">
+            <div class="event-date">{{ \Carbon\Carbon::parse($reservation->event->date)->format('d M, Y | h:i') }}</div>
+            <div class="ticket-price">${{ $reservation->event->price }}</div>
         </div>
-        <div class="card cardRight">
-            <div class="eye"></div>
-            <div class="number">
-                <h3>{{$reservation->id}}</h3>
-                <span>seat</span>
-            </div>
-            <div class="barcode"></div>
+        <h3 class="event-title">{{ $reservation->event->title }}</h3>
+        <div class="logo">
+            <p class="event-details">{{ $reservation->event->location }}</p>
+            <p class="client-name">{{ $reservation->user->name }}</p>
         </div>
-
+        <div class="logo">
+            <span>Seat N° : <strong>{{ $reservation->id }}</strong></span>
+            <p>
+              {{ $reservation->reservation_code }}
+            </p>
+        </div>
     </div>
 </body>
 
-<style>
-.cardWrap {
-  width: 27em;
-  margin: 3em auto;
-  color: #fff;
-  font-family: sans-serif;
-}
+</html>
 
-.card {
-  background: linear-gradient(to bottom, #e84c3d 0%, #e84c3d 26%, #ecedef 26%, #ecedef 100%);
-  height: 11em;
-  float: left;
-  position: relative;
-  padding: 1em;
-  margin-top: 100px;
-}
 
-.cardLeft {
-  border-top-left-radius: 8px;
-  border-bottom-left-radius: 8px;
-  width: 16em;
-}
 
-.cardRight {
-  width: 6.5em;
-  border-left: .18em dashed #fff;
-  border-top-right-radius: 8px;
-  border-bottom-right-radius: 8px;
-}
 
-.cardRight:before,
-.cardRight:after {
-  content: "";
-  position: absolute;
-  display: block;
-  width: .9em;
-  height: .9em;
-  background: #fff;
-  border-radius: 50%;
-  left: -.5em;
-}
 
-.cardRight:before {
-  top: -.4em;
-}
 
-.cardRight:after {
-  bottom: -.4em;
-}
 
-h1 {
-  font-size: 1.1em;
-  margin-top: 0;
-}
 
-h1 span {
-  font-weight: normal;
-}
 
-.title,
-.name,
-.seat,
-.time {
-  text-transform: uppercase;
-  font-weight: normal;
-}
 
-.title h2,
-.name h2,
-.seat h2,
-.time h2 {
-  font-size: .9em;
-  color: #525252;
-  margin: 0;
-}
+ --}}
 
-.title span,
-.name span,
-.seat span,
-.time span {
-  font-size: .7em;
-  color: #a2aeae;
-}
 
-.title {
-  margin: 2em 0 0 0;
-}
+ <!DOCTYPE html>
+<html lang="en">
 
-.name,
-.seat {
-  margin: .7em 0 0 0;
-}
+<head>
 
-.time {
-  margin: .7em 0 0 1em;
-}
+    <style>
+        /* Add your custom styles here */
 
-.seat,
-.time {
-  float: left;
-}
+        * {
+            box-sizing: border-box;
+            font-family: sans-serif;
+        }
 
-.eye {
-  position: relative;
-  width: 2em;
-  height: 1.5em;
-  background: #fff;
-  margin: 0 auto;
-  border-radius: 1em/0.6em;
-  z-index: 1;
-}
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
 
-.eye:before,
-.eye:after {
-  content: "";
-  display: block;
-  position: absolute;
-  border-radius: 50%;
-}
+        .ticket {
+            width: 95%;
+            padding: 0.3cm;
+            border: 2px solid #ddd;
+            background-color: rgba(242, 223, 242, 0.893);
+            display: flex;
+            flex-direction: column;
+        }
 
-.eye:before {
-  width: 1em;
-  height: 1em;
-  background: #e84c3d;
-  z-index: 2;
-  left: 8px;
-  top: 4px;
-}
+        .logo {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
 
-.eye:after {
-  width: .5em;
-  height: .5em;
-  background: #fff;
-  z-index: 3;
-  left: 12px;
-  top: 8px;
-}
+        .client-name{
+            font-weight: 700
+        }
 
-.number {
-  text-align: center;
-  text-transform: uppercase;
-}
+        .fill-violet-400 {
+            fill: rgb(246, 236, 42)
+        }
 
-.number h3 {
-  color: #e84c3d;
-  margin: .9em 0 0 0;
-  font-size: 2.5em;
-}
+        .event-info {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: center;
+        }
 
-.number span {
-  display: block;
-  color: #a2aeae;
-}
+        .event-date {
+            font-size: 1rem;
+            background-color: rgb(46, 92, 255);
+            color: #00ff22;
+            padding: 5px;
+            border-radius: 7px;
+        }
 
-.barcode {
-  height: 2em;
-  width: 0;
-  margin: 1.2em 0 0 .8em;
-  box-shadow: 1px 0 0 1px #343434,
-              5px 0 0 1px #343434,
-              10px 0 0 1px #343434,
-              11px 0 0 1px #343434,
-              15px 0 0 1px #343434,
-              18px 0 0 1px #343434,
-              22px 0 0 1px #343434,
-              23px 0 0 1px #343434,
-              26px 0 0 1px #343434,
-              30px 0 0 1px #343434,
-              35px 0 0 1px #343434,
-              37px 0 0 1px #343434,
-              41px 0 0 1px #343434,
-              44px 0 0 1px #343434,
-              47px 0 0 1px #343434,
-              51px 0 0 1px #343434,
-              56px 0 0 1px #343434,
-              59px 0 0 1px #343434,
-              64px 0 0 1px #343434,
-              68px 0 0 1px #343434,
-              72px 0 0 1px #343434,
-              74px 0 0 1px #343434,
-              77px 0 0 1px #343434,
-              81px 0 0 1px #343434;
-}
+        .event-title {
+            font-size: 18px;
+            font-weight: bold;
+        }
 
-</style>
+        .event-details {
+            font-size: 14px;
+            color: #777;
+            margin-bottom: 10px;
+        }
+
+        .ticket-price {
+            background-color: #eee;
+            padding: 5px 10px;
+            border-radius: 4px;
+            font-weight: bold;
+        }
+    </style>
+</head>
+
+<body style="width: 100%;">
+    <div class="ticket">
+        <div class="event-info">
+            <div class="event-date">{{ \Carbon\Carbon::parse($reservation->event->date)->format('d M, Y | h:i') }}</div>
+            <div class="ticket-price">${{ $reservation->event->price }}</div>
+        </div>
+        <h3 class="event-title">{{ $reservation->event->title }}</h3>
+        <div class="logo">
+            <p class="event-details">{{ $reservation->event->location }}</p>
+            <p class="client-name">{{ $reservation->user->name }}</p>
+        </div>
+        <div class="logo">
+            <span>Seat N° : <strong>{{ $reservation->id }}</strong></span>
+            <p>
+              {{ $reservation->reservation_code }}
+            </p>
+        </div>
+    </div>
+</body>
 
 </html>
