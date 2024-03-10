47af7524e2c3a38d6ae8b86c480481d77638bad8
lahcen <lahcen.ben3@gmail.com>
2024-03-10 17:03:49 +0000

    Update user permission display

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -32,10 +32,9 @@
                             @foreach ($user->userPermissions() as $permission)
                                 <span
                                     class="inline-flex items-center gap-1 rounded-full bg-blue-50 px-2 py-1 text-xs font-semibold text-blue-600">
-                                    {{ $permission['name'] }}
+                                    {{ $permission}}
                                 </span>
                             @endforeach
-
                         </div>
                     </div>
 
