f04ec72731721c325edf1012611ea66732629752
lahcen <lahcen.ben3@gmail.com>
2024-03-10 14:09:31 +0000

    Add new methods to ICategoryRepository interface

--- app/Repository/Interface/ICategoryRepository.php
+++ app/Repository/Interface/ICategoryRepository.php
@@ -5,6 +5,8 @@ namespace App\Repository\Interface;
 interface ICategoryRepository 
 {
     public function list(int $perPage = 10);
+    public function all();
+    public function random();
     public function findById($id);
     public function storeOrUpdate($data = [], $id = null);
     public function destroyById($id);
