565473a84ae3a0094b1c594e9ac781fd0d9c9130
lahcen <lahcen.ben3@gmail.com>
2024-03-10 14:11:36 +0000

    Add ReservationSeeder for database seeding

---
+++ database/seeders/ReservationSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Reservation;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ReservationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Reservation::factory()->count(50)->create();
+    }
+}
