24a1fd0f292f635b79269906649f3827629e7bcd
lahcen <lahcen.ben3@gmail.com>
2024-03-10 14:11:44 +0000

    Refactor RoleSeeder and Permission creation

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -3,58 +3,113 @@
 namespace Database\Seeders;
 
 use App\Models\Role;
-use App\Models\User;
-use App\Models\Permission;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Permission;
 
 class RoleSeeder extends Seeder
 {
     public function run(): void
     {
 
-        Role::create(['name' => 'admin'], ['name' => 'organizer'], ['name' => 'spectator']);
+        $admin = Role::create(['name' => 'admin']);
+        $organizer = Role::create(['name' => 'organizer']);
+        $spectator = Role::create(['name' => 'spectator']);
 
-        Permission::create(['name' => 'create-event'], ['name' => 'update-event'], ['name' => 'delete-event'], ['name' => 'view-event'], ['name' => 'view-any-event'], ['name' => 'create-category'], ['name' => 'update-category'], ['name' => 'delete-category'], ['name' => 'view-category'], ['name' => 'view-any-category'], ['name' => 'create-reservation'], ['name' => 'update-reservation'], ['name' => 'delete-reservation'], ['name' => 'view-reservation'], ['name' => 'view-any-reservation'], ['name' => 'create-user'], ['name' => 'update-user'], ['name' => 'delete-user'], ['name' => 'view-user'], ['name' => 'view-any-user'], ['name' => 'create-role'], ['name' => 'update-role'], ['name' => 'delete-role'], ['name' => 'view-role'], ['name' => 'view-any-role'], ['name' => 'create-permission'], ['name' => 'update-permission'], ['name' => 'delete-permission'], ['name' => 'view-permission'], ['name' => 'view-any-permission'],['access-dashboard'],['access-profile'],['access-settings'],['access-roles'],['access-permissions'],['access-users'],['access-events'],['access-reservations'],['access-categories'],['access-statistics'],['edit-profile']);
-        // $permission = new Permission();
-        // $permission->name = 'create-post';
-        // $permission->save();
+        Permission::create(['name' => 'create-event']);
+        Permission::create(['name' => 'update-event']);
+        Permission::create(['name' => 'delete-event']);
+        Permission::create(['name' => 'view-event']);
+        Permission::create(['name' => 'view-any-event']);
+        Permission::create(['name' => 'create-category']);
+        Permission::create(['name' => 'update-category']);
+        Permission::create(['name' => 'delete-category']);
+        Permission::create(['name' => 'view-category']);
+        Permission::create(['name' => 'view-any-category']);
+        Permission::create(['name' => 'create-reservation']);
+        Permission::create(['name' => 'update-reservation']);
+        Permission::create(['name' => 'delete-reservation']);
+        Permission::create(['name' => 'view-reservation']);
+        Permission::create(['name' => 'view-any-reservation']);
+        Permission::create(['name' => 'create-user']);
+        Permission::create(['name' => 'update-user']);
+        Permission::create(['name' => 'delete-user']);
+        Permission::create(['name' => 'view-user']);
+        Permission::create(['name' => 'view-any-user']);
+        Permission::create(['name' => 'create-role']);
+        Permission::create(['name' => 'update-role']);
+        Permission::create(['name' => 'delete-role']);
+        Permission::create(['name' => 'view-role']);
+        Permission::create(['name' => 'view-any-role']);
+        Permission::create(['name' => 'create-permission']);
+        Permission::create(['name' => 'update-permission']);
+        Permission::create(['name' => 'delete-permission']);
+        Permission::create(['name' => 'view-permission']);
+        Permission::create(['name' => 'view-any-permission']);
+        Permission::create(['name' => 'access-dashboard']);
+        Permission::create(['name' => 'access-profile']);
+        Permission::create(['name' => 'access-settings']);
+        Permission::create(['name' => 'access-roles']);
+        Permission::create(['name' => 'access-permissions']);
+        Permission::create(['name' => 'access-users']);
+        Permission::create(['name' => 'access-events']);
+        Permission::create(['name' => 'access-reservations']);
+        Permission::create(['name' => 'access-categories']);
+        Permission::create(['name' => 'access-statistics']);
+        Permission::create(['name' => 'edit-profile']);
+        Permission::create(['name' => 'restrict-access']);
+        Permission::create(['name' => 'accept-event']);
+        Permission::create(['name' => 'reject-event']);
+        Permission::create(['name' => 'publish-event']);
+        Permission::create(['name' => 'unpublish-event']);
+        Permission::create(['name' => 'view-published-events']);
+        Permission::create(['name' => 'view-rejected-events']);
+        Permission::create(['name' => 'view-all-events']);
+        Permission::create(['name' => 'view-all-reservations']);
+        Permission::create(['name' => 'view-all-users']);
+        Permission::create(['name' => 'view-all-categories']);
+  
 
-        // $role = new Role();
-        // $role->name = 'admin';
-        // $role->save();
-        // $role->permissions()->attach($permission);
-        // $permission->roles()->attach($role);
 
-        // $permission = new Permission();
-        // $permission->name = 'create-user';
-        // $permission->save();
 
-        // $role = new Role();
-        // $role->name = 'organizer';
-        // $role->save();
-        // $role->givePermissionsTo($permission);
-        // $permission->roles()->attach($role);
+        // $admin->givePermissionsTo([
+        //     'create-event',
+        //     'update-event',
+        //     'delete-event',
+        //     'view-event',
+        //     'view-any-event',
+        //     'create-category',
+        //     'update-category',
+        //     'delete-category',
+        //     'view-category',
+        //     'view-any-category',
+        //     'create-reservation',
+        //     'update-reservation',
+        //     'delete-reservation',
+        //     'view-reservation',
+        //     'view-any-reservation',
+        //     'create-user',
+        //     'update-user',
+        //     'delete-user',
+        //     'view-user',
+        //     'view-any-user',
+        //     'create-role'
+        // ]);
 
-        // $admin = Role::where('name', 'admin')->first();
-        // $userRole = Role::where('name', 'user')->first();
-        // $create_post = Permission::where('name', 'create-post')->first();
-        // $create_user = Permission::where('name', 'create-user')->first();
+        // $organizer->givePermissionsTo([
+        //     'create-event',
+        //     'update-event',
+        //     'delete-event',
+        //     'view-event',
+        //     'view-any-event',
+        //     'view-any-category',
+        //     'create-reservation',
+        //     'update-reservation',
+        //     'delete-reservation',
+        //     'view-reservation',
+        //     'edit-profile'
+        
+        // ]);
 
-        // $admin = new User();
-        // $admin->name = 'Admin';
-        // $admin->email = 'admin@gmail.com';
-        // $admin->password = bcrypt('admin');
-        // $admin->save();
-        // $admin->roles()->attach($admin);
-        // $admin->permissions()->attach($create_post);
 
-        // $user = new User();
-        // $user->name = 'User';
-        // $user->email = 'user@gmail.com';
-        // $user->password = bcrypt('user');
-        // $user->save();
-        // $user->roles()->attach($userRole);
-        // $user->permissions()->attach($create_user);
     }
 }
