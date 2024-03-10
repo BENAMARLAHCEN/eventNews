9bf15ac393f32cc209c5396b058e3878ad876319
lahcen <lahcen.ben3@gmail.com>
2024-03-10 14:10:48 +0000

    Add ReservationFactory for creating reservation instances

---
+++ database/factories/ReservationFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reservation>
+ */
+class ReservationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => rand(1, 10), 
+            'event_id' => rand(1, 20), 
+            'reservation_code' => $this->faker->uuid,
+        ];
+    }
+}
