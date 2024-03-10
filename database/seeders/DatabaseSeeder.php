b1e4205fcec7c7ca6d21065daaff2cd521f8baae
lahcen <lahcen.ben3@gmail.com>
2024-03-10 14:11:50 +0000

    Add model imports and update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // \App\Models\User::factory(10)->create();
+        // $this->call(RoleSeeder::class);
+        // $this->call(CategorySeeder::class);
+        // User::get()->each(function ($user) {
+        //     $user->roles()->attach(Role::where('name', 'organizer')->first());
+        // });
+        $this->call(EventSeeder::class);
+        // $this->call(ReservationSeeder::class);
+     
+        
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
