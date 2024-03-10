34368385551d8b4f0d93bbaafb955c983ff64f9a
lahcen <lahcen.ben3@gmail.com>
2024-03-10 17:03:28 +0000

    Update about page content

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,20 +1,26 @@
-
 @extends('layouts.app')
 
 @section('content')
-    <div class="bg-gray-200 py-8 px-4 sm:px-6 lg:px-8">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-                    <div class="text-2xl">
-                        About Page
-                    </div>
-
-                    <div class="mt-6 text-gray-500">
-                        <p>This is the about page content.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="bg-white rounded-lg shadow-lg p-8 max-w-4xl mx-auto mt-10">
+        <h1 class="text-3xl font-semibold text-gray-800 mb-6">About Us</h1>
+        <p class="text-gray-700 leading-relaxed">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec sem non erat feugiat finibus nec et velit. Sed
+            vitae sollicitudin lacus. Aliquam id arcu nec odio pharetra consectetur. Sed sed urna luctus, dapibus libero sit
+            amet, vestibulum felis. Duis at eros eget dui tincidunt posuere at non lectus. Vivamus dictum luctus massa vel
+            malesuada. Nulla sagittis semper neque, id lobortis turpis finibus id. Nullam tempus vitae lorem ac varius. In
+            interdum ultricies lorem non efficitur. Suspendisse potenti. Duis aliquam, tortor eu eleifend vestibulum, elit
+            turpis ultrices nisi, at fermentum metus velit ac magna.
+        </p>
+        <p class="text-gray-700 leading-relaxed mt-4">
+            Integer mollis mauris a fermentum varius. Nullam a nisi sapien. Curabitur nec augue et risus placerat molestie.
+            Integer fringilla sapien ut metus luctus, id congue dui vestibulum. Donec et libero vitae purus pellentesque
+            posuere. Mauris feugiat fringilla orci, sit amet posuere arcu tincidunt sed. Sed ut pharetra ipsum, id pharetra
+            tortor.
+        </p>
+        <p class="text-gray-700 leading-relaxed mt-4">
+            Nam ac libero magna. Morbi semper felis at mi pharetra, at tincidunt odio bibendum. Quisque nec mauris vel nulla
+            sodales vestibulum. Vivamus et urna at enim ultrices ultricies. Integer ut ex vitae sapien tristique lobortis
+            eget in libero. Praesent sodales semper magna non volutpat. Integer mattis malesuada turpis sit amet fermentum.
+        </p>
     </div>
 @endsection
