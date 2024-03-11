ab03c089c7c1e2c63fbbb6bd5d4c6c7dea3b9d62
lahcen <lahcen.ben3@gmail.com>
2024-03-11 12:17:25 +0000

    Refactor dashboard layout and add role-based access control

--- resources/views/layouts/dachboard.blade.php
+++ resources/views/layouts/dachboard.blade.php
@@ -93,20 +93,21 @@
                         <span class="mx-3">Event</span>
                     </a>
                 @endcan
+                @role('organizer')
+                    @can('organizer-view-events')
+                        <a class="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+                            href="{{ route('organizer.events.index') }}">
+                            <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z">
+                                </path>
+                            </svg>
 
-                @can('organizer-view-events')
-                    <a class="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
-                        href="{{ route('organizer.events.index') }}">
-                        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z">
-                            </path>
-                        </svg>
-
-                        <span class="mx-3">My Event</span>
-                    </a>
-                @endcan
+                            <span class="mx-3">My Event</span>
+                        </a>
+                    @endcan
+                
 
                 @can('organizer-view-reservations')
                     <a class="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
@@ -121,6 +122,7 @@
                         <span class="mx-3">Reservations</span>
                     </a>
                 @endcan
+                @endrole
                 @can('view-users')
                     <a class="flex items-center px-6 py-2 mt-4 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
                         href="{{ route('users.index') }}">
