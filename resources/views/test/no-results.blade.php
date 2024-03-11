ac9cccb1d8a7d24f594d52579f445bd48b511164
lahcen <lahcen.ben3@gmail.com>
2024-03-11 12:17:06 +0000

    Add no-results.blade.php view file

---
+++ resources/views/test/no-results.blade.php
@@ -0,0 +1,10 @@
+<div class="flex items-center justify-center my-8">
+    <div class="px-8 py-4 border rounded-lg border-gray-500/50 bg-red-50/50 dark:bg-slate-600/50">
+        <p class="leading-loose text-center text-gray-700 dark:text-gray-300">
+            <span class="mb-4 text-2xl font-bold md:text-4xl">No results found</span>
+            <br>
+            <br>
+            <span class="text-xl font-semibold">Please try again with different keywords</span>
+        </p>
+    </div>
+</div>
